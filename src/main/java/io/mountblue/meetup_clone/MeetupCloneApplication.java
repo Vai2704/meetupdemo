d17d0cfc46c5266666ff411b15367ec74b996aec
rishikesh <rishiborate24@gmail.com>
2025-02-20 15:52:53 +0530

    initial commit

---
+++ src/main/java/io/mountblue/meetup_clone/MeetupCloneApplication.java
@@ -0,0 +1,13 @@
+package io.mountblue.meetup_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mountblue/meetup_clone/MeetupCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mountblue.meetup_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
